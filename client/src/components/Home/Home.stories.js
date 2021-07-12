cf458cbcb7c5311182d4cfc4873b107ce517e16a
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-11 20:03:24 -0400

    add and configure generate-react-cli - use to gen Home component

---
+++ client/src/components/Home/Home.stories.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Home from "./Home";
+
+// Learn more about stories here: https://storybook.js.org/docs/react/get-started/introduction
+storiesOf("Home", module).add("default", () => <Home />);
